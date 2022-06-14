477d537d3f4400850f8ddb65413cfbd26e5a882e
owokaiser <101186844+owokaiser@users.noreply.github.com>
2022-06-14 11:21:42 +0000

    Commit 1

--- script.js
+++ script.js
@@ -15,7 +15,6 @@
   })
 
   cw1.addEventListener("click", function () {
-    answer.innerHTML="<h1>Loading...</h1>";
 fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => response.json())
        .then(function(dane) {
